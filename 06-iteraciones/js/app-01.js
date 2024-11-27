516247806143065dc2d8bcf8f82b254efebc9b94
Lamix301 <yayavalarezo@gmail.com>
2024-11-27 14:14:47 +0100

    Iteraciones

---
+++ 06-iteraciones/js/app-01.js
@@ -0,0 +1,23 @@
+//Sirven para repetir pasos
+//Mientras se cumpla una condición
+// let clicks = 0
+// while(true){
+//     alert("Has clicado " + clicks + " veces!, pringao");
+//     // clicks = clicks + 1;
+//     clicks++;
+// }
+const pin = "1234";
+let respuesta = "";
+let intentos = 0;
+
+while(respuesta !== pin && intentos < 3){
+    respuesta = prompt("Escribe un pin");
+    intentos++;
+}
+if (respuesta === pin) {
+    alert("El pin es correcto!!!");
+} else {
+    alert("Lo has intentado demasiadas veces.");
+}
+
+alert("El pin es correcto!!!");
