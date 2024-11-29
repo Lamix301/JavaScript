5cd8cace05d12c9fabf343c86eb794a7ba1c4737
Lamix301 <yayavalarezo@gmail.com>
2024-11-29 09:47:55 +0100

    Strings

---
+++ 07-strings/js/app-01.js
@@ -0,0 +1,8 @@
+//Comillas dobles
+let producto = "Monitor de '23' pulgadas";
+producto = 'Monitor de "23" pulgadas'
+//Escapar caracteres
+producto = "Moni\\tor de \"23\" pul\ngad\tas";
+
+//Saber la longitud de un string
+console.log(producto.length);
