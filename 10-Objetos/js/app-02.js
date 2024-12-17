6fc34ecf677ec61e76e3aea3dc8066039d518a63
Lamix301 <yayavalarezo@gmail.com>
2024-12-17 10:35:57 +0100

    Objetos js

---
+++ 10-Objetos/js/app-02.js
@@ -0,0 +1,31 @@
+//Los objetos pueden tener cualquier tipo de 
+//dato como valor de propiedad
+const producto = {
+    nombre: "Mesa comedor",
+    precio: 200,
+    stock:true,
+    color: ["negro", "blanco", "gris"],
+    info: {
+        peso: "16kg",
+        medidas: {
+            h: 2,
+            x: 2,
+            y: 2,
+        },
+    },
+};
+
+console.log(producto);
+console.log(`La ${producto.nombre} pesa ${producto.info.peso}`);
+
+function imprimirInfo(producto){
+    console,log(`La ${producto.nombre}
+    pesa ${producto.info.peso}`);
+    console.log("Tienes los siguientes colores: ");
+    for (let i = 0; i < producto.color.length; i++) {
+        const color = producto.color[i];
+        console.log(color);
+    }
+}
+imprimirInfo(producto);
+//Hacer una copia de un objeto
