39dd74bc0dedbc20b1d3679c9e1502b8729da88e
Lamix301 <yayavalarezo@gmail.com>
2025-01-08 10:03:05 +0100

    examinar DOM

---
+++ DOM/02-examinar-dom/js/script.js
@@ -0,0 +1,14 @@
+let out;
+
+out = document.all;
+out = document.all[li];
+out = document.all.length;
+
+//Todo el documento html
+out = document.documentElement;
+out = document.body;
+out = document.head;
+out = document.head.children;
+out = document.body.children;
+
+console.log(output)
