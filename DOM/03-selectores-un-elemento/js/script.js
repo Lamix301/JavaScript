23cb1d3a9c5a9e815655bc68749544235205d9da
Lamix301 <yayavalarezo@gmail.com>
2025-01-22 11:51:23 +0100

    DOM

--- DOM/03-selectores-un-elemento/js/script.js
+++ DOM/03-selectores-un-elemento/js/script.js
@@ -1,14 +1,12 @@
-let out;
+//Seleccionar el boton, el h1 y el input
+const miBoton = document.getElementById("mi-boton");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+const itemInput = document.querySelector("#item-input");
 
-out = document.all;
-out = document.all[li];
-out = document.all.length;
-
-//Todo el documento html
-out = document.documentElement;
-out = document.body;
-out = document.head;
-out = document.head.children;
-out = document.body.children;
-
-console.log(output)
+//añadirle un evento onclick
+miBoton.addEventListener("click", () => {
+  //Actualizar el contenido del h1 con el value del input
+  console.log("Has clicado en el botón");
+  tituloPrincipal.innerText = itemInput.value;
+  itemInput.value = ""; //Limpia el input de texto
+});
---
+++ DOM/09-proyecto-lista-compra/js/script.js
@@ -0,0 +1,186 @@
+//Traer las refeéncias de los elementos del DOM
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+const btnClear = document.querySelector("#clear");
+const itemFilter = document.querySelector("#filter");
+
+/**
+ * @description Obtiene los datos del LStorage y refresca la lista
+ */
+function displayItems() {
+  const lista = getItemsFromLocalStorage();
+  lista.forEach((item) => {
+    const li = createNewItem(item);
+    itemList.appendChild(li);
+  });
+  checkUI();
+}
+/**
+ *
+ * @param {SubmitEvent} evt
+ */
+function addItem(evt) {
+  evt.preventDefault(); //Previene el evento submit del formulario
+
+  //Validar que el input no esté vacio
+  const newItem = itemInput.value.trim();
+  //Si el campo está vacio avisamos al usuario y salimos de la funcion
+  if (newItem === "") {
+    alert("Por favor añade un tecto");
+    return;
+  }
+
+  //si el elemeto existe no se añade
+  if (checkIfItemExists(newItem) !== -1) {
+    alert("El item ya está en la lista");
+    itemInput.value = "";
+    return;
+  }
+
+  //Inserta el elemento en el DOM
+  const li = createNewItem(newItem);
+  itemList.appendChild(li);
+
+  //Añadir el elemento al localStorage
+  addItemToLocalStorage(newItem);
+
+  //Refrescamos el UI
+  checkUI();
+  //Limpiar el campo de texto
+  itemInput.value = "";
+}
+
+function removeItem(evt) {
+  if (evt.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Vas ha eliminar el item")) {
+      removeItemFromLocalStorage(
+        evt.target.parentElement.parentElement.textContent
+      );
+      evt.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  // itemList.innerHTML = "";
+  if (confirm("Vas ha eliminar toda la lista")) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    //limpiar el localStorage
+    localStorage.removeItem("lista");
+    checkUI();
+  }
+}
+function filterItems(evt) {
+  const text = evt.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.includes(text)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+/*********     Funciones para la creación de los elementos de la lista **********/
+function createNewItem(textItem) {
+  const li = document.createElement("li");
+  const text = document.createTextNode(textItem);
+  li.appendChild(text);
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  return li;
+}
+
+function createButton(clases) {
+  const button = document.createElement("button");
+  button.className = clases;
+
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(clases) {
+  const icon = document.createElement("i");
+  icon.className = clases;
+  return icon;
+}
+/**
+ * @description Habilita o desabilita los elementos graficos según si
+ * hay items en la lista o no
+ */
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    btnClear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    btnClear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+/***************   Local Storage functions ****************/
+function addItemToLocalStorage(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //añadir el nuevo item al array
+  itemsFromLocalStorage.push(item);
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
+function getItemsFromLocalStorage() {
+  let itemsFromLocalStorage;
+  if (localStorage.getItem("lista") === null) {
+    itemsFromLocalStorage = [];
+  } else {
+    itemsFromLocalStorage = JSON.parse(localStorage.getItem("lista"));
+  }
+  //Quiero devolver un array, no texto ni null
+  return itemsFromLocalStorage;
+
+  /*
+  const items = localStorage.getItem("lista");
+  if (items === null) {
+    return [];
+  }
+  return JSON.parse(items);
+  */
+}
+function removeItemFromLocalStorage(item) {
+  //Traer los datos del localStorage
+  let itemsFromLocalStorage = getItemsFromLocalStorage();
+  //eliminar el elemento del array
+  itemsFromLocalStorage = itemsFromLocalStorage.filter((i) => i !== item);
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
+/**
+ *
+ * @param {String} item
+ * @returns el indice del item si lo ecuentra, sinó -1
+ */
+function checkIfItemExists(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //Mira si existe
+  return itemsFromLocalStorage.indexOf(item);
+}
+
+//TODO funcion update
+
+//Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+btnClear.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+document.addEventListener("DOMContentLoaded", displayItems);
